3d140098c418c586b91cd3b01ee235e8b9839c0e
CorruptedKarma <karmii044@gmail.com>
2025-11-21 20:48:21 -0300

    lily update to be waterloaggeable and a little texture update

--- src/main/java/org/exodusstudio/arcana/common/block/water_lillie/lillie_block.java
+++ src/main/java/org/exodusstudio/arcana/common/block/water_lillie/lillie_block.java
@@ -12,31 +12,46 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.ScheduledTickAccess;
+import net.minecraft.world.level.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
-public class lillie_block extends Block {
+public class lillie_block extends Block implements SimpleWaterloggedBlock {
 
     private final DeferredBlock<sunken_lillie> waterLillieBlock;
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    private static final VoxelShape SHAPE = Block.column(4.0, 0.0, 6.0);
 
     public lillie_block(DeferredBlock<sunken_lillie> waterLilie, Properties properties) {
         super(properties);
         waterLillieBlock = waterLilie;
+        this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false));
     }
 
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
+    }
 
+    @Override
+    protected FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
 
     @Override
     protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
@@ -44,13 +59,29 @@ public class lillie_block extends Block {
             level.setBlock(pos, this.waterLillieBlock.get().defaultBlockState(), 3);
         }
 
+
+        BlockState newState = this.waterLillieBlock.get().defaultBlockState();
+
+
+        if (newState.hasProperty(WATERLOGGED)) {
+            newState.setValue(WATERLOGGED, state.getValue(WATERLOGGED));
+        }
+
     }
 
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
 
     @Override
     protected void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean movedByPiston) {
         if (!level.isClientSide()) {
             level.scheduleTick(pos, this, 20);
+        }
+
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
         }
     }
 
@@ -61,7 +92,24 @@ public class lillie_block extends Block {
             scheduledTickAccess.scheduleTick(pos, this, 20);
         }
 
-        return super.updateShape(state, level, scheduledTickAccess, pos, direction, neighborPos, neighborState, random);
+        if (state.getValue(WATERLOGGED)) {
+            scheduledTickAccess.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+
+        return state;
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        BlockPos pos = context.getClickedPos();
+        Level level = context.getLevel();
+        FluidState fluidState = level.getFluidState(pos);
+
+
+        boolean waterlogged = fluidState.getType() == Fluids.WATER;
+
+        return this.defaultBlockState().setValue(WATERLOGGED, waterlogged);
     }
 
     protected boolean scanForWater(BlockGetter level, BlockPos pos) {
